578e6416880f986bc087b5bdaf2d036e26da5f12
Matthew Hoggan <mehoggan@gmail.com>
2021-01-19 12:49:01 -0600

    [Libs and Includes] Libs and includes added.

---
+++ include/gw_micro_service/warscroll_collector/warscroll_collector.h
@@ -0,0 +1,13 @@
+#ifndef WARSCROLL_COLLECTOR_WARSCROLL_COLLECTOR_H_INCLUDED_
+#define WARSCROLL_COLLECTOR_WARSCROLL_COLLECTOR_H_INCLUDED_
+
+namespace gw_micro_service
+{
+  class warscroll_collector
+  {
+  public:
+    warscroll_collector();
+  };
+}
+
+#endif
---
+++ lib/warscroll_collector/warscroll_collector.cpp
@@ -0,0 +1,7 @@
+#include "gw_micro_service/warscroll_collector/warscroll_collector.h"
+
+namespace gw_micro_service
+{
+  warscroll_collector::warscroll_collector()
+  {}
+}
